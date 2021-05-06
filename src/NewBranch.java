6363fa1786a2e4a9a05df013f80cf7a65947501d
DH <1097088154@qq.com>
2021-05-06 14:01:04 +0800

    回滚

--- src/NewBranch.java
+++ src/NewBranch.java
@@ -5,5 +5,6 @@ public class NewBranch {
         System.out.println("测试Git用文件+++++++++++===========");
         System.out.println("产生冲突吧！！！");
         System.out.println("合并");
+        System.out.println("回滚掉");
     }
 }

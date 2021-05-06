ad02b5883bb3f26e8945f7ab929bd1e0e4f062ed
DH <1097088154@qq.com>
2021-05-06 10:15:52 +0800

    上传test31

--- src/NewBranch.java
+++ src/NewBranch.java
@@ -1,6 +1,9 @@
 public class NewBranch {
 
     public static void main(String[] args) {
+        System.out.println("测试Git用文件");
         System.out.println("测试Git用文件+++++++++++===========");
+        System.out.println("产生冲突吧！！！");
+        System.out.println("合并");
     }
 }
